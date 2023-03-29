b3286aec9bed3115ed516c46ca3f7394b6504d99
Inna Nomerovkina <inkosof@gmail.com>
2023-03-30 00:53:54 +0300

    begin 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,29 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const startBtn = document.querySelector('[data-start]')
+const timerEl = document.querySelector('.timer')
+
+ startBtn.disabled = true;
+
+
+
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      if (selectedDates[0] < defaultDate) {
+        Notiflix.Notify.failure("Please choose a date in the future");
+
+      } else {
+        
+    };
+  },
+};
+
+const fp = flatpickr('#datetime-picker', options);
